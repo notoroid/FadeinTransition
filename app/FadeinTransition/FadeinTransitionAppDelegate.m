e52a36f496ec4625f806a0bbf8f063326f676966
notoroid <noto@irimasu.com>
2011-05-24 00:41:17 +0900

    autorelease されたイメージへの対応

--- app/FadeinTransition/FadeinTransitionAppDelegate.m
+++ app/FadeinTransition/FadeinTransitionAppDelegate.m
@@ -23,55 +23,59 @@
 
 - (void) firedStart:(id)sender 
 {
-    CGFloat scale = [[UIScreen mainScreen] respondsToSelector:NSSelectorFromString(@"scale")] ? [UIScreen mainScreen].scale : 1.0f;
-        // スケールを判別
-    CGRect bounds = [UIScreen mainScreen].bounds;
-    
-    CGRect rectDrawArea = bounds;
-    UIGraphicsBeginImageContextWithOptions(CGSizeMake(rectDrawArea.size.width,rectDrawArea.size.height) , NO , scale );
-        // 画面サイズの描画領域を確保する
-    
-    [self.setupViewController.view.layer renderInContext:UIGraphicsGetCurrentContext()];
-       // 描画領域に対してsetupViewController のlayer の内容を描画する
-    
-    UIImage* screenImage = UIGraphicsGetImageFromCurrentImageContext();
-        // 作画した結果をUIImage として取得する
+    NSAutoreleasePool* autoreleasePool = [[NSAutoreleasePool alloc] init];
+    {
+        CGFloat scale = [[UIScreen mainScreen] respondsToSelector:NSSelectorFromString(@"scale")] ? [UIScreen mainScreen].scale : 1.0f;
+            // スケールを判別
+        CGRect bounds = [UIScreen mainScreen].bounds;
         
-    UIGraphicsEndImageContext(); // 確保した領域を解放する
-    self.setupViewController = nil;
-        // setupViewController はこのさき必要ないので解放する
-    
-    // セットアップ中viewControllerをメインビューを置き換え
-    self.window.rootViewController = self.navigationController;
+        CGRect rectDrawArea = bounds;
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(rectDrawArea.size.width,rectDrawArea.size.height) , NO , scale );
+            // 画面サイズの描画領域を確保する
+        
+        [self.setupViewController.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+           // 描画領域に対してsetupViewController のlayer の内容を描画する
+        
+        UIImage* screenImage = UIGraphicsGetImageFromCurrentImageContext();
+            // 作画した結果をUIImage として取得する
+            
+        UIGraphicsEndImageContext(); // 確保した領域を解放する
+        self.setupViewController = nil;
+            // setupViewController はこのさき必要ないので解放する
+        
+        // セットアップ中viewControllerをメインビューを置き換え
+        self.window.rootViewController = self.navigationController;
 
-    // アニメーションを開始
-    // 開始画面からメイン画面へフェードインしながら切り替わるアニメーションを実現する
-    
-    [CATransaction begin];
-    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-    
-    layerFade_ = [[CALayer alloc] init];
-    layerFade_.contents = (id)[screenImage CGImage];
-    
-    layerFade_.position = CGPointMake(bounds.size.width * .5f ,bounds.size.height * .5f + 20.0f);
-    layerFade_.bounds = CGRectMake(0.0f, 0.0f, rectDrawArea.size.width , rectDrawArea.size.height );
-    layerFade_.opacity = 0.0f;
-    [self.window.layer addSublayer:layerFade_];
-    
-    CABasicAnimation* theAnimation = [[CABasicAnimation alloc] init]; // アニメーションのインスタンスを確保
-    
-    theAnimation.duration = 1.0f; // アニメーションの秒数を指定 [秒].[ミリ秒]
-    theAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]; // アニメーションタイミングを登録
-    theAnimation.keyPath = @"opacity"; // 不透過度
-    theAnimation.fromValue = [NSNumber numberWithFloat:1.0f]; // 開始1.0
-    theAnimation.toValue = [NSNumber numberWithFloat:0.0f]; // 終了0.0
-    [theAnimation setValue:kSetupViewAnimationTypeFadeIn forKey:kSetupViewAnimationType]; // 独自のプロパティkSetupViewAnimationType に値kSetupViewAnimationTypeFadeIn を格納
-    theAnimation.delegate = self; // アニメーション終了後のdelegate を登録
-    [layerFade_ addAnimation:theAnimation forKey:@"fadeinAnimation"]; // キー名fadeinAnimation としてアニメーションを追加
-    
-    [theAnimation release]; // 追加が終わったアニメーションを削除
-    
-    [CATransaction commit ]; // CATransaction begin から始まったアニメーションをcommit する
+        // アニメーションを開始
+        // 開始画面からメイン画面へフェードインしながら切り替わるアニメーションを実現する
+        
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+        
+        layerFade_ = [[CALayer alloc] init];
+        layerFade_.contents = (id)[screenImage CGImage];
+        
+        layerFade_.position = CGPointMake(bounds.size.width * .5f ,bounds.size.height * .5f + 20.0f);
+        layerFade_.bounds = CGRectMake(0.0f, 0.0f, rectDrawArea.size.width , rectDrawArea.size.height );
+        layerFade_.opacity = 0.0f;
+        [self.window.layer addSublayer:layerFade_];
+        
+        CABasicAnimation* theAnimation = [[CABasicAnimation alloc] init]; // アニメーションのインスタンスを確保
+        
+        theAnimation.duration = 1.0f; // アニメーションの秒数を指定 [秒].[ミリ秒]
+        theAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]; // アニメーションタイミングを登録
+        theAnimation.keyPath = @"opacity"; // 不透過度
+        theAnimation.fromValue = [NSNumber numberWithFloat:1.0f]; // 開始1.0
+        theAnimation.toValue = [NSNumber numberWithFloat:0.0f]; // 終了0.0
+        [theAnimation setValue:kSetupViewAnimationTypeFadeIn forKey:kSetupViewAnimationType]; // 独自のプロパティkSetupViewAnimationType に値kSetupViewAnimationTypeFadeIn を格納
+        theAnimation.delegate = self; // アニメーション終了後のdelegate を登録
+        [layerFade_ addAnimation:theAnimation forKey:@"fadeinAnimation"]; // キー名fadeinAnimation としてアニメーションを追加
+        
+        [theAnimation release]; // 追加が終わったアニメーションを削除
+        
+        [CATransaction commit ]; // CATransaction begin から始まったアニメーションをcommit する
+    }
+    [autoreleasePool release];
 }
 
 // CAAnimation のdelegate メソッド
